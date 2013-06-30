38935d7c8882a3f14f86997c4600cc40e95cddc5
Olivier Halligon <olivier@halligon.net>
2013-06-30 16:39:23 +0200

    Fixed execute_after now using dedicated queue (Otherwise we have a deadlock on mainQueue, detected thanks to my Unit Tests)

--- OHHTTPStubs/OHHTTPStubs.m
+++ OHHTTPStubs/OHHTTPStubs.m
@@ -341,7 +341,7 @@ typedef OHHTTPStubsResponse*(^OHHTTPStubsRequestHandler)(NSURLRequest* request, 
 void execute_after(NSTimeInterval delayInSeconds, dispatch_block_t block)
 {
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), block);
+    dispatch_after(popTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block);
 }
 
 @end
