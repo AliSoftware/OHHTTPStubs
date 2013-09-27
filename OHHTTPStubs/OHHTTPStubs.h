4c54ddcaec5ef20163b7b7cfe07dec3c7036abfd
Olivier Halligon <olivier@halligon.net>
2013-09-27 02:06:09 +0200

    The methods now return an "id<OHHTTPStubsDescriptor>" instead of a plain OHHTTPStubsID. This new type allows to give stubs a name, for debugging purposes. We can also now list all installed stubs (with their name) and log when stubs are being activated by a request.
    This modification also allows us to gets rid of the old OHHTTPStubsRequestHandler typedef (marked as deprecated) and the historical architecture around "RequestHandlers" which will totally disappear in the next major version (no trace of the old concepts around "Request Handlers" anymore in future 3.0)

--- OHHTTPStubs/OHHTTPStubs.h
+++ OHHTTPStubs/OHHTTPStubs.h
@@ -34,7 +34,11 @@
 
 typedef BOOL(^OHHTTPStubsTestBlock)(NSURLRequest* request);
 typedef OHHTTPStubsResponse*(^OHHTTPStubsResponseBlock)(NSURLRequest* request);
-typedef id OHHTTPStubsID;
+
+@protocol OHHTTPStubsDescriptor
+/*! Arbitrary name that you can set and get to describe your stub. Use it as your own convenience. */
+@property(nonatomic, strong) NSString* name;
+@end
 
 ////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Interface
@@ -46,18 +50,23 @@ typedef id OHHTTPStubsID;
 #pragma mark - Class Methods
 
 /*! Dedicated method to add a stub
- @param testBlock Block that should return `YES` if the request passed as parameter should be stubbed with the response block, `NO` if it should hit the real world (or be managed by another stub).
+ @param testBlock Block that should return `YES` if the request passed as parameter should be stubbed with the response block,
+                  and `NO` if it should hit the real world (or be managed by another stub).
  @param responseBlock Block that will return the `OHHTTPStubsResponse` (response to use for stubbing) corresponding to the given request
- @return an opaque object that uniquely identifies the stub and can be later used to remove it with `removeStub:`
+ @return a stub descriptor that uniquely identifies the stub and can be later used to remove it with `removeStub:`.
+ @note The returned stub descriptor is retained (`__strong` reference) by `OHHTTPStubs` until it is removed
+       (with one of the `removeStub:`/`removeLastStub`/`removeAllStubs` methods); it is thus recommended to
+       keep it in a `__weak` storage (and not `__strong`) in your app code, to let the stub descriptor be destroyed
+       and let the variable go back to `nil` automatically when the stub is removed.
  */
-+(OHHTTPStubsID)stubRequestsPassingTest:(OHHTTPStubsTestBlock)testBlock
-                                     withStubResponse:(OHHTTPStubsResponseBlock)responseBlock;
++(id<OHHTTPStubsDescriptor>)stubRequestsPassingTest:(OHHTTPStubsTestBlock)testBlock
+                                   withStubResponse:(OHHTTPStubsResponseBlock)responseBlock;
 
 /*! Remove a stub from the list of stubs
- @param stubID the opaque object that has been returned when adding the stub using `stubRequestsPassingTest:withStubResponse:`
+ @param stubDesc the stub descriptor that has been returned when adding the stub using `stubRequestsPassingTest:withStubResponse:`
  @return `YES` if the stub has been successfully removed, `NO` if the parameter was not a valid stub identifier
  */
-+(BOOL)removeStub:(OHHTTPStubsID)stubID;
++(BOOL)removeStub:(id<OHHTTPStubsDescriptor>)stubDesc;
 
 /*! Remove the last added stub from the stubs list */
 +(void)removeLastStub;
@@ -70,6 +79,20 @@ typedef id OHHTTPStubsID;
  */
 +(void)setEnabled:(BOOL)enabled;
 
+#pragma mark - Debug Methods
+
+/*! List all the installed stubs
+ @return An array of id<OHHTTPStubsDescriptor> objects currently installed. Useful for debug.
+ */
++(NSArray*)allStubs;
+
+/*! Setup a block to be called each time a stub is triggered.
+ 
+ Useful if you want to log all your requests being stubbed for example and see which stub was used to respond to each request.
+  @param block The block to call each time a request is being stubbed by OHHTTPStubs. Set it to `nil` to do nothing. Defaults is `nil`.
+ */
++(void)onStubActivation:( void(^)(NSURLRequest* request, id<OHHTTPStubsDescriptor> stub) )block;
+
 @end
 
 
@@ -81,6 +104,8 @@ typedef id OHHTTPStubsID;
 @interface OHHTTPStubs (Deprecated)
 
 typedef id OHHTTPStubsRequestHandlerID __deprecated_msg("Use OHHTTPStubsDescriptor* instead");
+typedef id<OHHTTPStubsDescriptor> OHHTTPStubsID __deprecated_msg("Use id<OHHTTPStubsDescriptor> instead");
+
 
 /*! @warning This method is deprecated
  
