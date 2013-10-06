9268160eea2aefa5fb88fd975daf3b48e43e7f52
Olivier Halligon <olivier@halligon.net>
2013-10-06 22:18:19 +0200

    Code cleaning + more NSURLSession unit tests

--- OHHTTPStubs/UnitTests/Test Suites/NSURLSessionTests.m
+++ OHHTTPStubs/UnitTests/Test Suites/NSURLSessionTests.m
@@ -20,49 +20,128 @@
     [OHHTTPStubs removeAllStubs];
 }
 
-// Normal stubbing should work fine for the shared session
-- (void)test_DefaultSharedNSURLSession
+
+- (void)_test_NSURLSession:(NSURLSession*)session
+               jsonForStub:(id)json
+                completion:(void(^)(NSError* errorResponse,id jsonResponse))completion
 {
     if ([NSURLSession class])
     {
         static const NSTimeInterval kRequestTime = 1.0;
         static const NSTimeInterval kResponseTime = 1.0;
-        NSDictionary *expectedResponseDict = @{@"Success" : @"Yes"};
         
         [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
             return YES;
         } withStubResponse:^OHHTTPStubsResponse *(NSURLRequest *request) {
-            return [[OHHTTPStubsResponse responseWithJSONObject:expectedResponseDict statusCode:200 headers:nil]
+            return [[OHHTTPStubsResponse responseWithJSONObject:json statusCode:200 headers:nil]
                     requestTime:kRequestTime responseTime:kResponseTime];
         }];
         
-        NSURLSession *session = [NSURLSession sharedSession];
-        
         __block __strong id dataResponse = nil;
-        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://localhost:3333"]];
+        __block __strong NSError* errorResponse = nil;
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://unknownhost:666"]];
         [request setHTTPMethod:@"GET"];
         [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
         [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
         
         NSURLSessionDataTask *task =  [session dataTaskWithRequest:request
-                                                 completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-                                                     STAssertNil(error, @"Unexpected error");
-                                                     if (!error)
-                                                     {
-                                                         NSError *jsonError = nil;
-                                                         NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
-                                                         STAssertNil(jsonError, @"Unexpected error deserializing JSON response");
-                                                         dataResponse = jsonObject;
-                                                     }
-                                                     [self notifyAsyncOperationDone];
-                                                 }];
+                                                 completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+        {
+            errorResponse = error;
+            if (!error)
+            {
+                NSError *jsonError = nil;
+                NSDictionary *jsonObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+                STAssertNil(jsonError, @"Unexpected error deserializing JSON response");
+                dataResponse = jsonObject;
+            }
+            [self notifyAsyncOperationDone];
+        }];
 
         [task resume];
         
         [self waitForAsyncOperationWithTimeout:kRequestTime+kResponseTime+0.5];
         
-        STAssertEqualObjects(dataResponse, expectedResponseDict, @"Unexpected data received");
+        completion(errorResponse, dataResponse);
+    }
+}
+
+// Normal stubbing should work fine for the shared session
+- (void)test_SharedNSURLSession
+{
+    if ([NSURLSession class])
+    {
+        NSURLSession *session = [NSURLSession sharedSession];
         
+        NSDictionary* json = @{@"Success": @"Yes"};
+        [self _test_NSURLSession:session jsonForStub:json completion:^(NSError *errorResponse, id jsonResponse) {
+            STAssertNil(errorResponse, @"Unexpected error");
+            STAssertEqualObjects(jsonResponse, json, @"Unexpected data received");
+        }];
+    }
+    else
+    {
+        NSLog(@"Test skipped because the NSURLSession class is not available on this iOS version");
+    }
+}
+
+- (void)test_NSURLSessionDefaultConfig
+{
+    if ([NSURLSessionConfiguration class] && [NSURLSession class])
+    {
+        NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
+        [OHHTTPStubs setEnabled:YES forSessionConfiguration:config]; // Soon useless when swizzling in place
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+        
+        NSDictionary* json = @{@"Success": @"Yes"};
+        [self _test_NSURLSession:session jsonForStub:json completion:^(NSError *errorResponse, id jsonResponse) {
+            STAssertNil(errorResponse, @"Unexpected error");
+            STAssertEqualObjects(jsonResponse, json, @"Unexpected data received");
+        }];
+    }
+    else
+    {
+        NSLog(@"Test skipped because the NSURLSessionConfiguration & NSURLSession classes are not available on this iOS version");
+    }
+}
+
+- (void)test_NSURLSessionEphemeralConfig
+{
+    if ([NSURLSessionConfiguration class] && [NSURLSession class])
+    {
+        NSURLSessionConfiguration* config = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        [OHHTTPStubs setEnabled:YES forSessionConfiguration:config]; // Soon useless when swizzling in place
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+        
+        NSDictionary* json = @{@"Success": @"Yes"};
+        [self _test_NSURLSession:session jsonForStub:json completion:^(NSError *errorResponse, id jsonResponse) {
+            STAssertNil(errorResponse, @"Unexpected error");
+            STAssertEqualObjects(jsonResponse, json, @"Unexpected data received");
+        }];
+    }
+    else
+    {
+        NSLog(@"Test skipped because the NSURLSessionConfiguration & NSURLSession classes are not available on this iOS version");
+    }
+}
+
+- (void)test_NSURLSessionDefaultConfig_Disabled
+{
+    if ([NSURLSessionConfiguration class] && [NSURLSession class])
+    {
+        NSURLSessionConfiguration* config = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        [OHHTTPStubs setEnabled:NO forSessionConfiguration:config];
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+        
+        NSDictionary* json = @{@"Success": @"Yes"};
+        [self _test_NSURLSession:session jsonForStub:json completion:^(NSError *errorResponse, id jsonResponse) {
+            STAssertNotNil(errorResponse, @"Expected error but none found");
+            STAssertNil(jsonResponse, @"Data should not have been received as stubs should be disabled");
+        }];
+    }
+    else
+    {
+        NSLog(@"Test skipped because the NSURLSessionConfiguration & NSURLSession classes are not available on this iOS version");
     }
 }
 
