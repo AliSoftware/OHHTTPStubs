808e9e960ef84d8ca9d6aa026fd3ac4fbe026261
Olivier Halligon <olivier@halligon.net>
2013-12-15 20:08:34 +0100

    Travis Mac issue : trying a different “#if defined” approach

--- OHHTTPStubs/Sources/OHHTTPStubs.h
+++ OHHTTPStubs/Sources/OHHTTPStubs.h
@@ -86,8 +86,7 @@ typedef OHHTTPStubsResponse*(^OHHTTPStubsResponseBlock)(NSURLRequest* request);
  */
 +(void)setEnabled:(BOOL)enabled;
 
-#if (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 70000) \
- || (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 1090)
+#if defined(__IPHONE_7_0) || defined(__MAC_10_9)
 /*!
  @param enabled If `YES`, enables the stubs for this `NSURLSessionConfiguration`.
                 If `NO`, disable the stubs and let all the requests hit the real world
