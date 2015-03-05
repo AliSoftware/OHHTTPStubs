9b74c85180bc5d6229374763163e73f6f6a72e65
corinne krych <corinnekrych@gmail.com>
2015-03-05 10:14:49 +0100

    Add Pods and Podfile.lock

---
+++ OHHTTPStubsDemo/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// OHHTTPStubs
+#define COCOAPODS_POD_AVAILABLE_OHHTTPStubs
+#define COCOAPODS_VERSION_MAJOR_OHHTTPStubs 3
+#define COCOAPODS_VERSION_MINOR_OHHTTPStubs 1
+#define COCOAPODS_VERSION_PATCH_OHHTTPStubs 10
+
+// OHHTTPStubs/Core
+#define COCOAPODS_POD_AVAILABLE_OHHTTPStubs_Core
+#define COCOAPODS_VERSION_MAJOR_OHHTTPStubs_Core 3
+#define COCOAPODS_VERSION_MINOR_OHHTTPStubs_Core 1
+#define COCOAPODS_VERSION_PATCH_OHHTTPStubs_Core 10
+
