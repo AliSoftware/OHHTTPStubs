9f034a5bd06317270bcf711b66580a7b3fd5f183
Nolan O'Brien <info@nsprogrammer.com>
2015-04-02 14:51:52 -0700

    Remove unnecessary block indirection

--- OHHTTPStubs/Sources/OHHTTPStubs.m
+++ OHHTTPStubs/Sources/OHHTTPStubs.m
@@ -533,9 +533,7 @@ typedef struct {
 {
     dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        CFRunLoopPerformBlock(self.clientRunLoop, kCFRunLoopDefaultMode, ^{
-            block();
-        });
+        CFRunLoopPerformBlock(self.clientRunLoop, kCFRunLoopDefaultMode, block);
         CFRunLoopWakeUp(self.clientRunLoop);
     });
 }
