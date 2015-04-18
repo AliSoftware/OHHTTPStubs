a96d6e5e5d0447c37d4cefa2d73338610a048ad0
Olivier Halligon <olivier@halligon.net>
2015-04-18 20:59:43 +0200

    Initialize (empty) Swift project

---
+++ Examples/Swift/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OHHTTPStubsDemo
+//
+//  Created by Olivier Halligon on 18/04/2015.
+//  Copyright (c) 2015 AliSoftware. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet var delaySwitch: UISwitch?
+    @IBOutlet var textView: UITextView?
+    @IBOutlet var installTextStubSwitch: UISwitch?
+    @IBOutlet var imageView: UIImageView?
+    @IBOutlet var installImageStubSwitch: UISwitch?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
