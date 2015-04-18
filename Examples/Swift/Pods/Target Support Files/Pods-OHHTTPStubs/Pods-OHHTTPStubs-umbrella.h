7d5a95323e5489dc462d7e94ec84b4443c1f487d
Olivier Halligon <olivier@halligon.net>
2015-04-18 22:41:29 +0200

    Added OHHTTPStubs pod + MainViewController.swift code

---
+++ Examples/Swift/Pods/Target Support Files/Pods-OHHTTPStubs/Pods-OHHTTPStubs-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+#import "OHHTTPStubs.h"
+#import "OHHTTPStubsResponse+HTTPMessage.h"
+#import "OHHTTPStubsResponse+JSON.h"
+#import "OHHTTPStubsResponse.h"
+
+FOUNDATION_EXPORT double OHHTTPStubsVersionNumber;
+FOUNDATION_EXPORT const unsigned char OHHTTPStubsVersionString[];
+
