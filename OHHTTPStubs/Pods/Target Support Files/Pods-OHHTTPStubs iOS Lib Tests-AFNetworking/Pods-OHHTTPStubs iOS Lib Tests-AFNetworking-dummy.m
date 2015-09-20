2edec4c81ea72103736137e2a08e8cef9a10ca63
Olivier Halligon <olivier@halligon.net>
2015-09-20 11:20:33 +0200

    Fix main project Podfile (was referencing old target which was renamed since)

---
+++ OHHTTPStubs/Pods/Target Support Files/Pods-OHHTTPStubs iOS Lib Tests-AFNetworking/Pods-OHHTTPStubs iOS Lib Tests-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OHHTTPStubs_iOS_Lib_Tests_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_OHHTTPStubs_iOS_Lib_Tests_AFNetworking
+@end
