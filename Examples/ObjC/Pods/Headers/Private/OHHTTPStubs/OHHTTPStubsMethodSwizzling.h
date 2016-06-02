8c929af6f3769fdd66bb3bc293d22917debf77c3
AliSoftware <olivier@halligon.net>
2016-06-02 18:36:51 +0200

    Update ObjC example, with CocoaPods 1.0

---
+++ Examples/ObjC/Pods/Headers/Private/OHHTTPStubs/OHHTTPStubsMethodSwizzling.h
@@ -0,0 +1,1 @@
+../../../../../../OHHTTPStubs/Sources/NSURLSession/OHHTTPStubsMethodSwizzling.h
---
+++ Examples/ObjC/Pods/Target Support Files/Pods-OHHTTPStubsDemo/Pods-OHHTTPStubsDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OHHTTPStubsDemo : NSObject
+@end
+@implementation PodsDummy_Pods_OHHTTPStubsDemo
+@end
