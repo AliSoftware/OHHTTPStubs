1ecc4576e6488942a1682b4f6a8c9e58d53652f4
Liquidsoul <liquidsoul@liquidsoul.fr>
2016-08-18 08:29:55 +0200

    Update OHHTTPStubs dependency in Examples/ObjC

--- Examples/ObjC/Pods/Headers/Private/OHHTTPStubs/NSMutableURLRequest+HTTPBodyTesting.h
+++
@@ -1,1 +0,0 @@
-../../../../../../OHHTTPStubs/Sources/NSURLSession/NSMutableURLRequest+HTTPBodyTesting.h
---
+++ Examples/ObjC/Pods/Headers/Public/OHHTTPStubs/NSURLRequest+HTTPBodyTesting.h
@@ -0,0 +1,1 @@
+../../../../../../OHHTTPStubs/Sources/NSURLSession/NSURLRequest+HTTPBodyTesting.h
